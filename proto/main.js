80374fd83c50e846d44d38a30b6a6bb4326664e4
Jaime García <elgambitero@gmail.com>
2016-02-27 23:25:00 +0100

    Now the phone has info of all the screen. Divided resolution by 16 due to lack of power. Clean code

--- proto/main.js
+++ proto/main.js
@@ -7,57 +7,27 @@
 
 var camera = ui.addCameraView("back", 0, 0, 500, 500);
 var active = false;
-//var a = java.lang.reflect.Array.newInstance(java.lang.String, );
-
-function seekGreens(pixels) {
-    var sum = 0;
-    for(i=0;i>=pixels.length;i++){
-        for(j=0;i>=pixels[i].length;j++){
-            sum = sum + Color.green(pixels[i][j]);
-        }
-    }
-    quantity = pixels.length*pixels[0].length;
-    result = sum/quantity;
-    return result;
-    
-}
-
-ui.addToggle("Active",0,700,500,100,false).onChange(function(state){
-   active = !active;
-});
 
 ui.addButton("Test", 0, 800, 500, 100).onClick(function() { 
     
     camera.onNewBitmap(function(bitmap){ 
-        var pixel = 0;//java.lang.reflect.Array.newInstance(int, [bitmap.getWidth(),bitmap.getHeight()]);
-        pixel = bitmap.getPixel(0,0);
-        console.log('Value is: ' + pixel);
-        /*
-        if(seekGreens(pixels)>80){
+        var sum = 0.0;//java.lang.reflect.Array.newInstance(int, [bitmap.getWidth(),bitmap.getHeight()]);
+        //pixel = bitmap.getPixel(0,0);
+        for(i=0;i<bitmap.getHeight()/16;i++){
+            for(j=0;j<bitmap.getWidth()/16;j++){
+                sum = sum + (bitmap.getPixel(16*j,16*i) >> 8) & 0xFF;
+            }
+        }
+        
+        //pixel = (pixel >> 8) & 0xFF
+        var quantity = bitmap.getWidth()*bitmap.getHeight()/16;
+        var smems = sum/quantity;
+        console.log('Smems is: ' + smems);
+        if(smems>0.0005){
             camera.turnOnFlash(true);
         }else{
             camera.turnOnFlash(false);
-            
         }
-        */
+       
     });
 });
-
-/*
-if(active){
-    var pixels;
-    camera.onNewBitmap(function(bitmap){ 
-        bitmap.getPixels(pixels,0,3,0,0,bitmap.getWidth(),bitmap.getHeight());
-        if(seekGreens(pixels)>80){
-            camera.turnOnFlash(true);
-        }else{
-            camera.turnOnFlash(false);
-    }
-    });
-    
-    
-}else{
-    camera.turnOnFlash(false)
-}
-
-*/
