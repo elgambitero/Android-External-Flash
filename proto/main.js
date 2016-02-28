347934f2f7b2fc20eeaede9cb8c78d93b1c81a0e
Jaime García <elgambitero@gmail.com>
2016-02-28 01:16:20 +0100

    Work around protocoder's modulus operator inconsistencies

--- proto/main.js
+++ proto/main.js
@@ -1,3 +1,4 @@
+
 /*
 *	
 *	Description: When detecting a large amount of green light, the flash turns on.
@@ -8,18 +9,22 @@
 var camera = ui.addCameraView("back", 0, 0, 500, 500);
 var active = false;
 
-ui.addButton("Test", 0, 800, 500, 100).onClick(function() { 
-    
+function mod(a,b){
+    return a%b;   
+}
+
+
+function cycle(){
     camera.onNewBitmap(function(bitmap){ 
-        var sum = 0.0;//java.lang.reflect.Array.newInstance(int, [bitmap.getWidth(),bitmap.getHeight()]);
-        //pixel = bitmap.getPixel(0,0);
+        
+        var sum = 0.0;
         for(i=0;i<bitmap.getHeight()/512;i++){
             for(j=0;j<bitmap.getWidth()/512;j++){
-                sum = sum - (bitmap.getPixel(512*j,512*i)/256)%256
+            sum = sum - mod((bitmap.getPixel(512*j,512*i)/256),256);
             }
         }
         
-        //pixel = (pixel >> 8) & 0xFF
+        
         var quantity = bitmap.getWidth()*bitmap.getHeight()/512.0;
         var smems = sum/quantity;
         console.log('Smems is: ' + smems +', ' + bitmap.getWidth());
@@ -30,5 +35,10 @@ ui.addButton("Test", 0, 800, 500, 100).onClick(function() {
         }
        
     });
+}
+
+ui.addButton("Begin", 0, 800, 500, 100).onClick(function() { 
+    cycle();
 });
+
 
